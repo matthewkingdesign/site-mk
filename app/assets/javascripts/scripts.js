c32e6aa9064f7a585d5346806338c00bc40fae53
matthewkingdesign <hello@matthewking.design>
2018-08-01 05:41:54 +0100

    added custom cursor - called it via action in jquery

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
   // $("#homepage-hamburger").click(function() {
   //   $("#homepage-blurb").fadeToggle();
   // });
+  // $("#homepage-hamburger").hover(function(){
+  //   if($(this).hasClass("zoomed")){
+  //     $(this).removeClass("zoomed");
+  //   }else{
+  //     $(this).addClass("zoomed");
+  //   }
+  // });
+
+  $("#homepage-hamburger").click(function(){
+  if($(".container").hasClass("zoomed")){
+    $(".container").removeClass("zoomed");
+  }else{
+      $(".container").addClass("zoomed");
+  }
+  });
 });
